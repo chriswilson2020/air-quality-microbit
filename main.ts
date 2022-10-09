f5bdf05e6eb04ff769551c93a845e01f1fee5125
chriswilson2020 <73828727+chriswilson2020@users.noreply.github.com>
2022-10-09 17:24:27 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,34 +1,18 @@
-let Temperature = 0
 kitronik_air_quality.setTime(16, 19, 0)
 kitronik_air_quality.setDate(9, 10, 2022)
 kitronik_air_quality.setupGasSensor()
+kitronik_air_quality.calcBaselines()
 loops.everyInterval(60000, function () {
     kitronik_air_quality.measureData()
     kitronik_air_quality.clear()
-    kitronik_air_quality.show("Environment" + kitronik_air_quality.readTime())
-    kitronik_air_quality.show("Tmp:  " + ("" + kitronik_air_quality.readTemperature(kitronik_air_quality.TemperatureUnitList.C)) + "°C", 3)
-    kitronik_air_quality.show("Press:" + ("" + kitronik_air_quality.readPressure(kitronik_air_quality.PressureUnitList.Pa)) + " Pa", 4)
-    kitronik_air_quality.show("Hum:  " + ("" + kitronik_air_quality.readHumidity()) + "    %", 5)
-    kitronik_air_quality.show("CO2:  " + ("" + kitronik_air_quality.readeCO2()) + "   ppm", 6)
-    kitronik_air_quality.show("IAQ:  " + ("" + kitronik_air_quality.getAirQualityPercent()) + "    %", 7)
+    kitronik_air_quality.show("Environment at: " + kitronik_air_quality.readTime())
+    kitronik_air_quality.show("Temperature:  " + ("" + kitronik_air_quality.readTemperature(kitronik_air_quality.TemperatureUnitList.C)) + "°C", 3)
+    kitronik_air_quality.show("Pressure:     " + ("" + kitronik_air_quality.readPressure(kitronik_air_quality.PressureUnitList.Pa)) + " Pa", 4)
+    kitronik_air_quality.show("Humidity:     " + ("" + kitronik_air_quality.readHumidity()) + "    %", 5)
+    kitronik_air_quality.show("CO2:          " + ("" + kitronik_air_quality.readeCO2()) + "   ppm", 6)
+    kitronik_air_quality.show("IAQ:          " + ("" + kitronik_air_quality.getAirQualityScore()) + "    /500", 7)
+    kitronik_air_quality.show("IAQ:          " + ("" + kitronik_air_quality.getAirQualityPercent()) + "    %", 8)
 })
 basic.forever(function () {
-    Temperature = kitronik_air_quality.readTemperature(kitronik_air_quality.TemperatureUnitList.C)
-    if (Temperature >= 16) {
-        basic.showLeds(`
-            # . # . #
-            . # # # .
-            # # # # #
-            . # # # .
-            # . # . #
-            `)
-    } else {
-        basic.showLeds(`
-            # . . . #
-            . # # # .
-            . # . # .
-            . # # # .
-            # . . . #
-            `)
-    }
+	
 })
